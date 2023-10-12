b62728b5d9350dfc40e39a9bb5995c1f64a03c77
Alan-Graton <graton.alan@gmail.com>
2023-10-12 14:45:00 -0300

    init commit

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export function Groups() {
+    return (
+        <>
+            <View>
+                <Text>I'm the 'Groups' Screen</Text>
+            </View>
+        </>
+    )
+}
