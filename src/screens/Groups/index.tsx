1b3b98dd4aaa96264dc1f076d3c04e0980bf3185
Alan-Graton <graton.alan@gmail.com>
2023-10-12 19:39:23 -0300

    wip: using created components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,39 @@
-import * as Styled from "./styles";
-// import { Container, Title } from "./styles"; - Components are clearner this way
+import React from "react";
+// Screen Components
+import { GroupCards } from "@/components/GroupCards";
+// Application Components
+import { AppHeader } from "@/components/AppHeader";
+import { AppHighlight } from "@/components/AppHighlight";
+import { AppButton } from "@/components/AppButton";
+import { AppEmptyList } from "@/components/AppEmptyList";
+// Native
+import { FlatList } from "react-native";
+// Styles
+import * as S from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = React.useState<string[]>([]);
+
   return (
     <>
-      <Styled.Container>
-        <Styled.Title>I'm the 'Groups' Screen</Styled.Title>
-      </Styled.Container>
+      <S.Container>
+        <AppHeader />
+        <AppHighlight.root>
+          <AppHighlight.title title="Turmas" />
+          <AppHighlight.subtitle subtitle="Jogue com a sua Turma" />
+        </AppHighlight.root>
+        <FlatList
+          data={groups}
+          keyExtractor={(item) => item}
+          renderItem={() => <GroupCards />}
+          ListEmptyComponent={() => (
+            <AppEmptyList.root>
+              <AppEmptyList.subtitle subtitle="Que tal criar uma?" />
+            </AppEmptyList.root>
+          )}
+        />
+        <AppButton text="Criar Turma" />
+      </S.Container>
     </>
   );
 }
