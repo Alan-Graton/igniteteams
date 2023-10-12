5bb4b0d87a2d3c54d2addc5232eee1e15a90d947
Alan-Graton <graton.alan@gmail.com>
2023-10-12 15:54:48 -0300

    wip: init styling with Styled-Components and minor adjusts

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -4,6 +4,11 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+
+  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
 `;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  font-size: ${({ theme }) => theme.FONT_SIZE.LG}px;
+`;
