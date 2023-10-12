f9e161ad432454520de5eb5cefd3da6eec4922d7
Alan-Graton <graton.alan@gmail.com>
2023-10-12 19:37:29 -0300

    wip: minor adjusts to layout(for now)

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,10 +2,10 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
 
-  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+  padding: 24px;
+
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
 `;
 
 export const Title = styled.Text`
