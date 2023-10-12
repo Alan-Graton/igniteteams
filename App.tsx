1963f4ba3439dd785bb803cfa7a784c244eb97d8
Alan-Graton <graton.alan@gmail.com>
2023-10-12 15:55:00 -0300

    wip: minor adjustments

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
-import { StatusBar } from "expo-status-bar";
+// Screens
 import { Groups } from "@/screens/Groups";
+// Components
+import { StatusBar } from "expo-status-bar";
+// Styles
 import { ThemeProvider } from "styled-components/native";
 import theme from "./src/theme";
 
@@ -7,7 +10,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <Groups />
-      <StatusBar style="auto" />
+      <StatusBar style="inverted" translucent />
     </ThemeProvider>
   );
 }
