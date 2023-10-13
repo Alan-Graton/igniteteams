04bc9b88f2d25902f6443de6605b3fae3416aaa2
Alan-Graton <graton.alan@gmail.com>
2023-10-12 23:22:51 -0300

    fix: minor adjusts to AppHighlight text alignment

--- src/components/AppHighlight/styles.ts
+++ src/components/AppHighlight/styles.ts
@@ -16,6 +16,7 @@ export const Title = styled.Text`
 `;
 
 export const Subtitle = styled.Text`
+  text-align: center;
   font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
   color: ${({ theme }) => theme.COLORS.GRAY_300};
   font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
