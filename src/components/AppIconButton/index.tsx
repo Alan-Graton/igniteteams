b3a492f5953059e378f5f09b8031d3b0f3e12215
Alan-Graton <graton.alan@gmail.com>
2023-10-14 14:54:01 -0300

    fix: solve crashing bug, wip

--- src/components/AppIconButton/index.tsx
+++ src/components/AppIconButton/index.tsx
@@ -4,14 +4,14 @@ import { MaterialIcons } from "@expo/vector-icons";
 import * as S from "./styles";
 
 type Props = TouchableOpacityProps & {
-  type?: S.IconButtonTypeStyleProp;
   icon: keyof typeof MaterialIcons.glyphMap;
+  type?: S.IconButtonTypeStyleProp;
 };
 
 export function AppIconButton({ icon, type = "PRIMARY", ...rest }: Props) {
   return (
     <S.Container {...rest}>
-      <S.Icon name={icon} color={"green"} size={32} />
+      <S.Icon name={icon} type={type} />
     </S.Container>
   );
 }
--- src/components/AppIconButton/styles.ts
+++ src/components/AppIconButton/styles.ts
@@ -22,4 +22,4 @@ export const Container = styled(TouchableOpacity)`
 export const Icon = styled(MaterialIcons).attrs<Props>(({ theme, type }) => ({
   size: 24,
   color: type === "PRIMARY" ? theme.COLORS.GREEN_700 : theme.COLORS.RED,
-}));
+}))``;
